31571b0a401b9615112978c58a7718dfc41b7939
Tiago Damasceno <devtiago01@gmail.com>
2023-02-18 22:25:21 -0300

    v 0.0.5

--- automato.py
+++ automato.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
+import pyautogui
+
+
 serviço = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service = serviço)
-
+print name
 navegador.get('https://www.conquistandosuavaga.com/pre-inscricao')
-navegador.find_elements('xpath', '//*[@id="mauticform_input_99formulariopesquisaaposc10_eu_sou_o_pedro_marins_e_v"]').send_keys('tiago')
+navegador.find_elements('xpath', '//*[@id="mauticform_input_99formulariopesquisaaposc10_eu_sou_o_pedro_marins_e_v"]')
 
