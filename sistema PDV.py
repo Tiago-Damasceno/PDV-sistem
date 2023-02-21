0a566c28fba2e6f82bd21ef57ebae160f7c95df6
Tiago Damasceno <devtiago01@gmail.com>
2023-02-20 23:38:34 -0300

    v 0.0.1

---
+++ sistema PDV.py
@@ -0,0 +1,7 @@
+# 1 criar uma lista de produtos
+# 2 colocar preços e codigos a essa lista
+# 3 hospedar essa lista em uma classe (SE POSSIVEL A PESSOA
+# PODE FAZER A PROPRIA CLASSE)
+# 4 fazer um banco de dados atualizavel para essa lista.
+# 5 criar uma interface para melhor visualização dessa lista
+# 
