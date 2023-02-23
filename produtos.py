792bfcd865335228f1c13253d92b8a5dd137f586
Tiago Damasceno <devtiago01@gmail.com>
2023-02-22 23:46:01 -0300

    v 0.0.4

--- produtos.py
+++ produtos.py
@@ -1,10 +1,10 @@
 class produtos:
-    def lista_de_produtos(self, produto, codigo, preço):
+    def __init__(self, produto, codigo, preço) -> None:
         self.produto = produto
         self.codigo = codigo
         self.preço = preço
 
 
 
-produto = produtos.lista_de_produtos('hamburger', '001', '10,00')
+produto = produtos('hamburger', '001', '10,00')
 print(produto.preço)
